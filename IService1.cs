0927375ca8f17abcc52faf7c4f9e2f8411023787
Enzo Jesús Esteban Quispe <enzoest26@gmail.com>
2023-12-14 14:27:50 -0500

    Proyecto WCF Libreria

---
+++ IService1.cs
@@ -0,0 +1,308 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace WCFServicioLibreria
+{
+    // NOTA: puede usar el comando "Rename" del menú "Refactorizar" para cambiar el nombre de interfaz "IService1" en el código y en el archivo de configuración a la vez.
+    [ServiceContract]
+    public interface IService1
+    {
+
+        [OperationContract]
+        string GetData(int value);
+
+        [OperationContract]
+        CompositeType GetDataUsingDataContract(CompositeType composite);
+
+        // TODO: agregue aquí sus operaciones de servicio
+        [OperationContract]
+        List<DtoLibro> SP_LISTARVISTALIBROS();
+
+        [OperationContract]
+        void SP_RESERVARLIBRO(Reservation reservation);
+
+        [OperationContract]
+        User SP_VALIDARACCESO(string email, string password);
+
+        [OperationContract]
+        int SP_VALIDARRESERVA(int idLibro);
+    }
+
+
+    // Utilice un contrato de datos, como se ilustra en el ejemplo siguiente, para agregar tipos compuestos a las operaciones de servicio.
+    [DataContract]
+    public class CompositeType
+    {
+        bool boolValue = true;
+        string stringValue = "Hello ";
+
+        [DataMember]
+        public bool BoolValue
+        {
+            get { return boolValue; }
+            set { boolValue = value; }
+        }
+
+        [DataMember]
+        public string StringValue
+        {
+            get { return stringValue; }
+            set { stringValue = value; }
+        }
+    }
+
+    public class User
+    {
+
+        private int _idUser;
+        [DataMember]
+        public int idUser
+        {
+            get { return _idUser; }
+            set { _idUser = value; }
+        }
+
+        private string _varFirstName;
+        [DataMember]
+        public string varFirstName
+        {
+            get { return _varFirstName; }
+            set { _varFirstName = value; }
+        }
+
+        private string _varLastName;
+        [DataMember]
+        public string varLastName
+        {
+            get { return _varLastName; }
+            set { _varLastName = value; }
+        }
+
+        private string _varEmail;
+        [DataMember]
+        public string varEmail
+        {
+            get { return _varEmail; }
+            set { _varEmail = value; }
+        }
+
+        private string _varPassword;
+        [DataMember]
+        public string varPassword
+        {
+            get { return _varPassword; }
+            set { _varPassword = value; }
+        }
+
+        private int _intStatus;
+        [DataMember]
+        public int intStatus
+        {
+            get { return _intStatus; }
+            set { _intStatus = value; }
+        }
+
+        private DateTime _dmeDateCreate;
+        [DataMember]
+        public DateTime dmeDateCreate
+        {
+            get { return _dmeDateCreate;}
+            set { _dmeDateCreate = value;}
+        }
+
+        private DateTime? _dmeDateUpdate;
+        [DataMember]
+        public DateTime? dmeDateUpdate
+        {
+            get { return _dmeDateUpdate;}
+            set { _dmeDateUpdate = value;}
+        }
+
+        private Boolean _bolIsActive;
+        [DataMember]
+        public Boolean bolIsActive
+        {
+            get { return _bolIsActive; }
+            set { _bolIsActive = value; }
+        }
+    }
+
+    public class Book
+    {
+        private int _idBook;
+        [DataMember]
+        public int idBook
+        {
+            get { return _idBook; }
+            set { _idBook = value; }
+        }
+
+        private string _varTitle;
+        [DataMember]
+        public string varTitle
+        {
+            get { return _varTitle; }
+            set { _varTitle = value; }
+        }
+
+        private string _varCode;
+        [DataMember]
+        public string varCode
+        {
+            get { return _varCode; }
+            set { _varCode = value; }
+        }
+
+        private int _intStatus;
+        [DataMember]
+        public int intStatus
+        {
+            get { return _intStatus; }
+            set { _intStatus = value; }
+        }
+
+        private DateTime _dmeDateCreate;
+        [DataMember]
+        public DateTime dmeDateCreate
+        {
+            get { return _dmeDateCreate; }
+            set { _dmeDateCreate = value; }
+        }
+
+        private DateTime _dmeDateUpdate;
+        [DataMember]
+        public DateTime dmeDateUpdate
+        {
+            get { return _dmeDateUpdate; }
+            set { _dmeDateUpdate = value; }
+        }
+
+        private Boolean _bolIsReservated;
+        [DataMember]
+        public Boolean bolIsReservated
+        {
+            get { return _bolIsReservated; }
+            set { _bolIsReservated = value; }
+        }
+
+        private Boolean _bolIsActive;
+        [DataMember]
+        public Boolean bolIsActive
+        {
+            get { return _bolIsActive; }
+            set { _bolIsActive = value; }
+        }
+    }
+
+    public class Reservation
+    {
+        private int _idReservation;
+        [DataMember]
+        public int idReservation
+        {
+            get { return _idReservation; }
+            set { _idReservation = value; }
+        }
+        private int _idUser;
+        [DataMember]
+        public int idUser
+        {
+            get { return _idUser; }
+            set { _idUser = value; }
+        }
+
+        private int _idBook;
+        [DataMember]
+        public int idBook
+        {
+            get { return _idBook; }
+            set { _idBook = value; }
+        }
+
+        private DateTime _dmeDateReservation;
+        [DataMember]
+        public DateTime dmeDateReservation
+        {
+            get { return _dmeDateReservation; }
+            set { _dmeDateReservation = value; }
+        }
+
+        private int _intStatus;
+        [DataMember]
+        public int intStatus
+        {
+            get { return _intStatus; }
+            set { _intStatus = value; }
+        }
+
+        private DateTime _dmeDateCreate;
+        [DataMember]
+        public DateTime dmeDateCreate
+        {
+            get { return _dmeDateCreate; }
+            set { _dmeDateCreate = value; }
+        }
+
+        private DateTime _dmeDateUpdate;
+        [DataMember]
+        public DateTime dmeDateUpdate
+        {
+            get { return _dmeDateUpdate; }
+            set { _dmeDateUpdate = value; }
+        }
+
+        private Boolean _bolIsActive;
+        [DataMember]
+        public Boolean bolIsActive
+        {
+            get { return _bolIsActive; }
+            set { _bolIsActive = value; }
+        }
+    }
+
+    public class DtoLibro
+    {
+        private int _idItem;
+        public int idItem
+        {
+            get { return _idItem; }
+            set { _idItem = value; }
+        }
+
+        private int _idBook;
+        [DataMember]
+        public int idBook
+        {
+            get { return _idBook; }
+            set { _idBook = value; }
+        }
+
+        private string _varTitle;
+        [DataMember]
+        public string varTitle
+        {
+            get { return _varTitle; }
+            set { _varTitle = value; }
+        }
+        private DateTime? _dmeDateReservation;
+        [DataMember]
+        public DateTime? dmeDateReservation
+        {
+            get { return _dmeDateReservation; }
+            set { _dmeDateReservation = value; }
+        }
+
+        private string _varStatus;
+        [DataMember]
+        public string varStatus
+        {
+            get { return _varStatus; }
+            set { _varStatus = value; }
+        }
+    }
+}
---
+++ Service1.svc.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using System.Data;
+
+namespace WCFServicioLibreria
+{
+    // NOTA: puede usar el comando "Rename" del menú "Refactorizar" para cambiar el nombre de clase "Service1" en el código, en svc y en el archivo de configuración.
+    // NOTE: para iniciar el Cliente de prueba WCF para probar este servicio, seleccione Service1.svc o Service1.svc.cs en el Explorador de soluciones e inicie la depuración.
+    public class Service1 : IService1
+    {
+
+        SqlConnection cn;
+
+        public SqlConnection conectar()
+        {
+            try
+            {
+                cn = new SqlConnection("SERVER=.;DATABASE=dbReto1;Integrated Security=SSPI");
+                cn.Open();
+                return cn;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public List<DtoLibro> SP_LISTARVISTALIBROS()
+        {
+            conectar();
+            SqlCommand cmd = new SqlCommand("SP_LISTARVISTALIBROS", cn);
+            cmd.CommandType = CommandType.StoredProcedure;
+
+            List<DtoLibro> lstDtoLibros = new List<DtoLibro>();
+            try
+            {
+                SqlDataReader reader = cmd.ExecuteReader();
+                int item = 0;
+                while (reader.Read())
+                {
+                    DtoLibro dtoLibro = new DtoLibro
+                    {
+                        idItem = item,
+                        idBook = reader.GetInt32(0),
+                        varTitle = reader.GetString(1),
+                        varStatus = reader.GetString(2),
+                        dmeDateReservation = !reader.IsDBNull(3) ? (DateTime?)reader.GetDateTime(3) : null
+
+                    };
+                    item++;
+                    lstDtoLibros.Add(dtoLibro);
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            return lstDtoLibros;
+        }
+
+        public void SP_RESERVARLIBRO(Reservation reservation)
+        {
+            conectar();
+            SqlCommand cmd = new SqlCommand("SP_RESERVARLIBRO", cn);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.Add("@ID_USER", SqlDbType.Int).Value = reservation.idUser;
+            cmd.Parameters.Add("@ID_BOOK", SqlDbType.VarChar).Value = reservation.idBook;
+            cmd.Parameters.Add("@DATE_RESERVATION", SqlDbType.DateTime).Value = reservation.dmeDateReservation;
+            cmd.Parameters.Add("@DATE_CREATED", SqlDbType.DateTime).Value = reservation.dmeDateCreate;
+            cmd.Parameters.Add("@STATUS", SqlDbType.Int).Value = reservation.intStatus;
+            cmd.Parameters.Add("@ACTIVE", SqlDbType.Bit).Value = reservation.bolIsActive;
+           
+            try
+            {
+               cmd.ExecuteNonQuery();
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public User SP_VALIDARACCESO(string email, string password)
+        {
+            conectar();
+            SqlCommand cmd = new SqlCommand("SP_VALIDARACCESO", cn);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.Add("@EMAIL", SqlDbType.VarChar).Value = email;
+            cmd.Parameters.Add("@PASSWORD", SqlDbType.VarChar).Value = password;
+            User user = new User();
+            try
+            {
+                SqlDataReader reader = cmd.ExecuteReader();
+                if (reader.HasRows)
+                {
+                    while (reader.Read())
+                    {
+                        user.idUser = reader.GetInt32(0);
+                        user.varFirstName = reader.GetString(1);
+                        user.varLastName = reader.GetString(2);
+                        user.varEmail = reader.GetString(3);
+                        user.varPassword = reader.GetString(4);
+                        user.intStatus = reader.GetInt32(5);
+                        user.dmeDateCreate = reader.GetDateTime(6);
+                        user.dmeDateUpdate = !reader.IsDBNull(7) ? (DateTime?)reader.GetDateTime(7) : null;
+                        user.bolIsActive = reader.GetBoolean(8);
+                    }
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            return user;
+        }
+
+        public int SP_VALIDARRESERVA(int idLibro)
+        {
+            conectar();
+            SqlCommand cmd = new SqlCommand("SP_VALIDARRESERVA", cn);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.Add("@ID_BOOK", SqlDbType.Int).Value = idLibro;
+            int verdad = 1; //0 - No hay reserva y 1 - Hay Reserva
+            try
+            {
+                SqlDataReader reader = cmd.ExecuteReader();
+                while(reader.Read())
+                {
+                    verdad = reader.GetInt32(0);
+                }
+            }
+            catch (Exception) 
+            { 
+                throw;
+            }
+            return verdad;
+        }
+
+
+        public string GetData(int value)
+        {
+            return string.Format("You entered: {0}", value);
+        }
+
+        public CompositeType GetDataUsingDataContract(CompositeType composite)
+        {
+            if (composite == null)
+            {
+                throw new ArgumentNullException("composite");
+            }
+            if (composite.BoolValue)
+            {
+                composite.StringValue += "Suffix";
+            }
+            return composite;
+        }
+    }
+}
